6fc8bee188f9e40d0bba334427ec69ef38cced4e
arvindalawa07 <arvindalawa07@gmail.com>
2022-08-21 22:40:24 +0530

    msg

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,19 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyC1UmgeQ9Y7jChoT-IGCKp96_XkNvuqk50",
-    authDomain: "linkedin-clone-1cfd5.firebaseapp.com",
-    projectId: "linkedin-clone-1cfd5",
-    storageBucket: "linkedin-clone-1cfd5.appspot.com",
-    messagingSenderId: "599309015813",
-    appId: "1:599309015813:web:9872da018895e13e09b56c",
-    measurementId: "G-XK4FS86BJ8"
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId:process.env.REACT_APP_APPID,
+    measurementId:process.env.REACT_APP_MEASUREMENTID
   };
   const firebaseApp=firebase.initializeApp(firebaseConfig);
   const db=firebaseApp.firestore();
   const auth=firebase.auth();
 const provider=new firebase.auth.GoogleAuthProvider();
 const storage=firebase.storage();
+
 export {auth,provider,storage};
 export default db;
